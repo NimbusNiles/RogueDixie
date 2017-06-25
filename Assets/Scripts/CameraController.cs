0bc2f4adfae5ac992aa94caf9209c7dfdc826e08
TimmPure <T.j.m.voskuil@gmail.com>
2017-06-25 12:56:48 +0200

    Failure to resolve Issue #15 Camera unproperly clamped due to variable aspect ratio. Possibly use ScreenToViewportPoint or Camera width/height in pixels to solve?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     private GameObject player;
+    private Transform mapSize;
+    private Vector3 mapPosition;
+    private Vector3 mapScale;
+    private float cameraSize;
     private Vector3 offset;
+    private float camClampMinX;
+    private float camClampMinY;
+    private float camClampMaxX;
+    private float camClampMaxY;
+    private Vector3 camClamp;
 
-	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<Player>().gameObject;
+        mapSize = FindObjectOfType<MapSize>().transform;
+        mapPosition = mapSize.position;
+        mapScale = mapSize.localScale;
+        cameraSize = GetComponent<Camera>().orthographicSize;
+
+        camClampMinX = mapPosition.x - .5f * mapScale.x + cameraSize;
+        camClampMinY = mapPosition.y - .5f * mapScale.y + cameraSize;
+        camClampMaxX = mapPosition.x + .5f * mapScale.x - cameraSize;
+        camClampMaxY = mapPosition.y + .5f * mapScale.y - cameraSize;
+
         offset.x = 1f / 32f;
         offset.y = 1f / 32f;
         offset.z = -10f;
     }
 	
-	// Update is called once per frame
-	void Update () {
-        transform.position = player.transform.position + offset;
+	void LateUpdate () {
+        camClamp.x = Mathf.Clamp(player.transform.position.x + offset.x, camClampMinX, camClampMaxX);
+        camClamp.y = Mathf.Clamp(player.transform.position.y + offset.y, camClampMinY, camClampMaxY);
+        camClamp.z = player.transform.position.z + offset.z;
+        transform.position = camClamp;
     }
 }
