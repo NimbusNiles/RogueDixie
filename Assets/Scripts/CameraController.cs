5a0789099385170631b2b943566a970a8948dccd
TimmPure <T.j.m.voskuil@gmail.com>
2017-06-25 15:49:50 +0200

    Closed Issue #15: camera restricted to the play space Aspect ratio is recalculated each frame, so resizing mid-game no longer breaks the follow bounds Tidied up code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,35 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
     private GameObject player;
     private Transform mapSize;
+    private Camera thisCamera;
+
     private Vector3 mapPosition;
     private Vector3 mapScale;
-    private float cameraSize;
     private Vector3 offset;
+    private Vector3 camClamp;
+
     private float camClampMinX;
     private float camClampMinY;
     private float camClampMaxX;
     private float camClampMaxY;
-    private Vector3 camClamp;
+
     private float cameraAspect;
+    private float cameraSize;
 
-    //TODO: tidy up (e.g. put camera component in a variable), call camera.ResetAspect() to fix camera issues after resizing
-
-	void Start () {
+    void Start () {
         player = FindObjectOfType<Player>().gameObject;
         mapSize = FindObjectOfType<MapSize>().transform;
-        mapPosition = mapSize.position;
-        mapScale = mapSize.localScale + new Vector3(3f,3f,0); //compensating for the fact that the tiles lie 3 units outside the MapSize object in both directions
-        cameraSize = GetComponent<Camera>().orthographicSize;
-        cameraAspect = GetComponent<Camera>().aspect;
+        thisCamera = GetComponent<Camera>();
+
+        mapPosition = mapSize.position;                         //Used as offset
+        mapScale = mapSize.localScale + new Vector3(3f,3f,0);   //Extra 3 units padding, compensating for the fact that the tiles lie 1.5 units outside the MapSize object in both directions
+        cameraSize = thisCamera.orthographicSize;               //Size of the camera is the -height- in world space units
+        cameraAspect = thisCamera.aspect;                       //Using aspect ratio to determine the -width- in world space units
 
         camClampMinX = mapPosition.x - .5f * mapScale.x + cameraSize * cameraAspect;
         camClampMaxX = mapPosition.x + .5f * mapScale.x - cameraSize * cameraAspect;
-
         camClampMinY = mapPosition.y - .5f * mapScale.y + cameraSize;
         camClampMaxY = mapPosition.y + .5f * mapScale.y - cameraSize;
 
@@ -39,9 +40,15 @@ public class CameraController : MonoBehaviour {
     }
 	
 	void LateUpdate () {
-        camClamp.x = Mathf.Clamp(player.transform.position.x + offset.x, camClampMinX, camClampMaxX);
-        camClamp.y = Mathf.Clamp(player.transform.position.y + offset.y, camClampMinY, camClampMaxY);
-        camClamp.z = player.transform.position.z + offset.z;
+        thisCamera.ResetAspect();
+        cameraAspect = thisCamera.aspect;
+
+        camClampMinX = mapPosition.x - .5f * mapScale.x + cameraSize * cameraAspect;
+        camClampMaxX = mapPosition.x + .5f * mapScale.x - cameraSize * cameraAspect;
+
+        camClamp = (player.transform.position + offset);
+        camClamp.x = Mathf.Clamp(camClamp.x, camClampMinX, camClampMaxX);
+        camClamp.y = Mathf.Clamp(camClamp.y, camClampMinY, camClampMaxY);
         transform.position = camClamp;
     }
 }
