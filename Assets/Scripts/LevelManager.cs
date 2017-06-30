dbddead36e8955251c59b4c3d5a7e5563eb371b1
Niels Petterson <nielspetterson@gmail.com>
2017-06-30 10:07:42 +0200

    Working prototype of Start and Options menu
     - Buttons work but are very ugly
     - Scaling is not yet implemented

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	void LoadNextLevel() {
-        
+	public void LoadNextLevel() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+
+    public void LoadLevel(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit Requested");
+        Application.Quit();
     }
 }
