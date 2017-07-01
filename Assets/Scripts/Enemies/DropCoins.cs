6ee6d0a7f37cf3fcf3617016c79cb94dbbe2651e
TimmPure <T.j.m.voskuil@gmail.com>
2017-07-01 14:41:20 +0200

    Enemies drop coins on death
    Enemies have a DropCoins.cs
    On death, a gold coin is instantiated at the enemy's transform.location

---
+++ Assets/Scripts/Enemies/DropCoins.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCoins : MonoBehaviour {
+
+    public GameObject coinPrefab;
+    
+    public void Drop() {
+        Instantiate(coinPrefab, transform.position, Quaternion.identity);
+    }
+}
