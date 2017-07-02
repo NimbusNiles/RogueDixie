bac941198fa4c5cb8b0496395adbc5b5a365957b
Niels Petterson <nielspetterson@gmail.com>
2017-07-02 16:16:07 +0200

    Weapon UI is created
     - Bottom left of the screen features a UI element which shows an icon of the current weapon.
     - When a weapon is equipped, this icon is updated.

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour {
     public static InventoryController Instance { get; set; }
     public List<Item> playerItems = new List<Item>();
+
+    public delegate void WeaponEquipAction(Item weapon);
+    public static event WeaponEquipAction OnWeaponEquip;
 
     private void Start() {
         if (Instance != null && Instance != this) {
@@ -27,6 +31,9 @@ public class InventoryController : MonoBehaviour {
 
     public void EquipWeapon(Item weaponToEquip) {
         GetComponent<PlayerWeaponController>().EquipWeapon(weaponToEquip);
+        if(OnWeaponEquip != null) {
+            OnWeaponEquip(weaponToEquip);
+        }
     }
 
 }
