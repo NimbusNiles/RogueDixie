e169211d71da8dde141163fbe18a4e85cd34794e
Niels Petterson <nielspetterson@gmail.com>
2017-07-05 13:00:56 +0200

    Refactored the player resource scripts
     - The PlayerHUD script now also handles the updating of the resource bar.
     - A new script is responsible to keep track of player resource (PlayerResource.cs)
    
    Known Issues:
    
     - Due to execution order, the PlayerHud script tries to update the resource bar before it has found it in his start method.

--- Assets/Scripts/Menus/PlayerHUD.cs
+++ Assets/Scripts/Menus/PlayerHUD.cs
@@ -6,21 +6,27 @@ using UnityEngine.UI;
 public class PlayerHUD : MonoBehaviour {
 
     public GameObject wastedScreen;
+    public GameObject resourceBarObj;
 
     private Healthbar healthBar;
+    private Healthbar resourceBar;
 
     private void OnEnable() {
         PlayerHealth.OnPlayerDeath += EnableWastedScreen;
         PlayerHealth.OnPlayerDamage += UpdateHealthBar;
+        PlayerResource.OnResourceChange += UpdateResourceBar;
     }
 
     private void OnDisable() {
         PlayerHealth.OnPlayerDeath -= EnableWastedScreen;
         PlayerHealth.OnPlayerDamage -= UpdateHealthBar;
+        PlayerResource.OnResourceChange -= UpdateResourceBar;
     }
 
     private void Start() {
         healthBar = GetComponentInChildren<Healthbar>();
+        resourceBar = resourceBarObj.GetComponentInChildren<Healthbar>();
+        SetResourceColor();
     }
 
     void EnableWastedScreen() {
@@ -29,6 +35,29 @@ public class PlayerHUD : MonoBehaviour {
 
     void UpdateHealthBar(float damage, float currentHealth, float maxHealth) {
         healthBar.SetHealthbar(currentHealth, maxHealth);
+    }
+
+    void UpdateResourceBar(float currentResource, float maxResource) {
+        resourceBar.SetHealthbar(currentResource, maxResource);
+    }
+
+    void SetResourceColor() {
+        PlayerResource myResource = FindObjectOfType<PlayerResource>();
+        switch (myResource.MyResourceType) {
+            case PlayerResource.ResourceTypes.Mana:
+                resourceBar.GetComponent<Image>().color = Color.blue;
+                break;
+            case PlayerResource.ResourceTypes.Rage:
+                resourceBar.GetComponent<Image>().color = new Color(155f/255f, 0f, 189f/255f);
+                break;
+            case PlayerResource.ResourceTypes.Stamina:
+                resourceBar.GetComponent<Image>().color = new Color(62f/255f, 210f/255f, 136f/255f);
+                break;
+            default:
+                Debug.Log("ResourceType not recognized");
+                break;
+        }
+
     }
 
 }
